b0715d61dff4cae55cff03492c67a1d2296509dd
root <root@DESKTOP-D9GDID1.localdomain>
2020-02-07 17:04:56 -0700

    Added an array of function pointers that hold the 4 actions such as add, divide, etc. Created an array of function pointers that holds the magnitude and phase. Still work in progress for final completion.

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "my_complex.h"
 
+
 int main() {
 	Complex a = {3, 4};
 	Complex b = {5, 12};
-
+	Complex (*spell[4])(Complex, Complex) = {complex_add, complex_sub, complex_mult, complex_div};
+	double (*wizard[2])(double, double) = {magnitude, phase};
 	return 0;
 }
