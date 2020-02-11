9188d04456f77c8fb568f7d912c474f4fc9ba266
root <root@DESKTOP-E9CHIFN.localdomain>
2020-02-10 22:35:12 -0700

    Revised the pointer names with numbers to distinguish the difference between the two. Plus, added 2 for loops for the functions, along with a declaration of int x for the 'for' loop. Changed the two double inputs to a 'Complex' as it, not only removed errors and allowed the program to run, but also correclty coorilated with its initial function in the fns.c file.

--- main.c
+++ main.c
@@ -5,7 +5,23 @@
 int main() {
 	Complex a = {3, 4};
 	Complex b = {5, 12};
-	Complex (*spell[4])(Complex, Complex) = {complex_add, complex_sub, complex_mult, complex_div};
-	double (*wizard[2])(double, double) = {magnitude, phase};
+	Complex (*fn_world[4])(Complex, Complex) = {complex_add, complex_sub, complex_mult, complex_div};
+	double (*fn_world1[2])(Complex) = {magnitude, phase};
+	
+	int x; 
+	
+	for(x=0; x<4; x++) {
+		
+		world(fn_world[x], a , b);
+	}
+
+
+	for(x=0; x<2; x++) {
+
+		Hat(fn_world1[x], a);
+		
+		Hat(fn_world1[x], b);
+	}
+
 	return 0;
 }
