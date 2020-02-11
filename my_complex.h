8384c4cef9891987abd45fc0955d18c4241d9baf
root <root@DESKTOP-E9CHIFN.localdomain>
2020-02-10 22:27:48 -0700

    Revised the pointer names with numbers to distinguish the the difference between the two. Plus, it cleared up errors that occured.

--- my_complex.h
+++ my_complex.h
@@ -20,5 +20,5 @@ double magnitude(Complex c);
 double phase(Complex c);
 
 void world(Complex(*fn_world)(Complex,Complex), Complex x, Complex y);
-void Hat(double (*fn_world)(Complex), Complex cat);
+void Hat(double (*fn_world1)(Complex), Complex cat);
 
